b14b0baedf0e92801ef5d0d73ad45b9da74c9d55
MatchaKim <kimjunsik333@gmail.com>
2025-03-05 13:32:33 +0900

    feat : snack bar 임시구현

--- AppSdk/src/screens/MainScreen/MainScreen.tsx
+++ AppSdk/src/screens/MainScreen/MainScreen.tsx
@@ -1,13 +1,38 @@
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, Modal, Keyboard } from 'react-native';
 import { MainScreenProps } from './MainScreen.props';
-
+import React, { useState } from 'react';
+import { TextInput } from 'react-native-gesture-handler';
+import { KeyboardAvoidingView, Platform } from 'react-native';
 const MainScreen: React.FC<MainScreenProps> = ({ navigation }) => {
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    const handleModal = () => {
+        setIsModalVisible(true);
+        setTimeout(() => {
+            setIsModalVisible(false);
+        }, 1000);
+    }
     return (
-        <View>
-            <Button
-                title="Webview"
+        <View
+            style={{ flex: 1, alignItems: 'center' }}
+        >
+
+                <Button
+                    title="Webview"
                 onPress={() => navigation.navigate('WebviewScreen', { url: 'http://localhost:5173' })}
             />
+            <Button
+                title="Modal"
+                onPress={handleModal}
+            />
+            <TextInput
+                style={{ borderWidth: 1, borderColor: 'black', width: '100%', height: 40 }}
+                placeholder="Input"
+            />
+            {isModalVisible &&
+                <View style={{ padding: 10, justifyContent: 'center', alignItems: 'center', backgroundColor: 'red',position:'absolute', bottom: 0 }}>
+                    <Text>Hello</Text>
+                </View>
+            }
         </View>
     )
 }
